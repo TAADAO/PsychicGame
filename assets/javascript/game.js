82f296279c3aa86fcee3d7ee8e14788e94f0022f
Robert O <romohund@gmail.com>
2018-12-05 21:17:20 -0600

    Not 100% perfect, but a pretty good working version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,8 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 
 var wins = 0;
 var losses = 0;
+var allowedGuesses = 10;
+
 
 
 var directionsText = document.getElementById("directions-text");
@@ -10,25 +12,49 @@ var userChoiceText = document.getElementById("userchoice-text");
 var computerChoiceText = document.getElementById("computerchoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text")
+var allowedGuessesText = document.getElementById("allowedGuesses-text")
 
-{
+
+function psychicGame() {
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     document.onkeyup = function (event) {
 
         var userGuess = event.key;
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
 
         if (userGuess === computerGuess) {
             wins++;
-        } else {
+            allowedGuesses = 10;
+            userGuess = "";
+            computerChoiceText.textContent = "Congratulations";
+            psychicGame();
+        }
+
+        else if (allowedGuesses > 0) {
+            allowedGuesses--
+        }
+        else {
             losses++;
+            allowedGuesses = 10;
+            userGuess = "";
+            computerChoiceText.textContent = "Your computer chose: " + computerGuess;
+            psychicGame();
         }
 
         directionsText.textContent = "";
-        
-        userChoiceText.textContent = "You chose: " + userGuess;
-        computerChoiceText.textContent = "The computer chose: " + computerGuess;
+
+        allowedGuessesText.textContent = "Guesses left: " + allowedGuesses;
         winsText.textContent = "wins: " + wins;
         lossesText.textContent = "losses: " + losses;
 
+        if (allowedGuesses === 10) {
+            userChoiceText.textContent = "Your Guesses so far: " + userGuess;
+        }
+        else {
+            userChoiceText.textContent += ", " + userGuess;
+        }
+
     }
 };
+
+psychicGame()
